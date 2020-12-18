57286b71e8b5d6f00afec5480888f5de7bd13531
Utsav Pandey <utsavpandey8@gmail.com>
2020-12-18 19:41:30 +0530

    redux persist added

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,19 @@ import './styles/css/Index.css';
 import { Provider } from 'react-redux'
 import App from './App';
 import store from './store';
-import {saveState} from './store/localStorage'
+// import persistor from './store'
+import {PersistGate} from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
+
+
+export const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={ persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,38 +1,32 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { loadState,saveState } from './localStorage'; 
 import authReducer from './reducers/authReducer';
 import { todoReducer } from './reducers/listReducer';
+import logger from 'redux-logger';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
-const persistedState = loadState();
+const middlewares = [logger];
 
-function saveToLocalStorage(state:any) {
-  const serializedState = JSON.stringify(state);
-  localStorage.setItem('state', serializedState);
+export const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['list']
 }
-
-function loadFromLocalStorage() {
-const serializedState = localStorage.getItem('state');
-if (serializedState === null) return undefined;
- return JSON.parse(serializedState);
-}
-
-const presistedState = loadFromLocalStorage();
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   list: todoReducer,
 });
 
+export const persisted = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
-  presistedState,
-  composeWithDevTools(applyMiddleware(thunk)),
+  persisted,
+  composeWithDevTools(applyMiddleware(...middlewares,thunk)),
 )
 
 export type RootState = ReturnType<typeof rootReducer>;
-store.subscribe(() => saveToLocalStorage(store.getState()));
-// store.subscribe(() => saveState({ list: store.getState() }))
 
 export default store;
